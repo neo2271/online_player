5738557262c841f18f529115bee1b96832cf27b3
NeO_TanDT <neo2271@gmail.com>
2021-01-10 23:37:47 +0700

    Initalize

---
+++ script.js
@@ -0,0 +1,12 @@
+// Change the second argument to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('video', { captions: { active: true } });
+
+// Expose player so it can be used from the console
+window.player = player;
+
+function watch() {
+    var x = document.getElementById("url").value;
+    document.getElementById("mp4_url").innerHTML = x;
+    document.getElementById("video").src = x;
+  }
